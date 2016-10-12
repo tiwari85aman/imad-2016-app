c5c57fa7d34228fd2074e800edd4b9a9833d91a1
tiwari85aman <tiwari.aman85@gmail.com>
2016-10-12 23:45:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res) {
+    res.send('Article One Requested');
+});
+
+app.get('/article-two', function(req, res) {
+    res.send('Article Two Requested');
+});
+
+app.get('/article-three', function(req, res) {
+    res.send('Article Three Requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
